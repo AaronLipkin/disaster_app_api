13e6c760311c807a477d741f90596e8bb8586bed
Aaron Lipkin <aj17@me.com>
2017-09-13 04:40:56 -0500

    added city update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :cities, only: [:index, :show]
+  resources :cities, only: 
   resources :survivors
   post '/survivors/distanced', to: 'survivors#distanced'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
